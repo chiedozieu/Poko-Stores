1bcc8f5870329d10418e6dcf36a57587c3a117c9
chiedozieu <chiedozieugwor@gmail.com>
2024-06-17 21:43:06 +0100

    basic setup, pages and header component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
-    <div className='text-2xl text-red-800'>
-      Home
-    </div>
+   <BrowserRouter>
+      <Header />
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/signup' element={<SignUp />}/>
+      <Route path='/signin' element={<SignIn />}/>
+    </Routes>
+     <Footer />
+   </BrowserRouter>
   )
 }
 
