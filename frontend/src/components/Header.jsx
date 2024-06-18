93c3ea23e8b9755e030ee4b6b266d0c6b83a08ae
chiedozieu <chiedozieugwor@gmail.com>
2024-06-18 22:39:13 +0100

    add logo, search, user and cart to header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { GrSearch } from "react-icons/gr";
+import Logo from "./Logo";
+import { PiUser } from "react-icons/pi";
+import { BsCart4 } from "react-icons/bs";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header className="h-[6rem] shadow-md">
+     
+      <div className=" h-full container mx-auto flex items-center px-4 justify-between">
+          <div className="">
+            <Logo />
+          </div>
+     
+        
+         <div className="hidden md:inline-flex items-center w-full justify-between max-w-md h-12 border rounded-full focus-within:shadow pl-2">
+            <input type="text" placeholder="Search products" className=" w-full outline-none "/>
+            <div>
+              <GrSearch  className="bg-red-700 h-full text-lg min-w-[50px] p-2 rounded-r-full text-white flex justify-center items-center"/>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className=" cursor-pointer text-3xl">
+            <PiUser />
+            </div>
+            <div className="text-3xl relative">
+              <span>
+                <BsCart4 />
+              </span>
+             <div className="bg-red-700 text-white w-5 h-5 p-1 flex items-center justify-center rounded-full absolute -top-1 -right-2 ">
+               <p className="text-sm">0</p>
+             </div>
+            </div>
+          </div>
+       </div>
+    </header>
+
+    </>
   )
 }
 
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Logo() {
+  return (
+    <div className='flex relative w-[5rem]'>
+        <img src="poko.png" alt="logo" className="object-cover w-full"/>
+    </div>
+  )
+}
