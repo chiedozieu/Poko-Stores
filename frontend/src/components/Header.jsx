f64190cebf2abd1753aede9c880b2b28d6121f87
chiedozieu <chiedozieugwor@gmail.com>
2024-06-19 06:32:52 +0100

    complete header ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,27 +15,34 @@ const Header = () => {
           </div>
      
         
-         <div className="hidden md:inline-flex items-center w-full justify-between max-w-md h-12 border rounded-full focus-within:shadow pl-2">
+         <div className="hidden md:inline-flex items-center w-full justify-between max-w-md h-12 border border-red-700 rounded-full focus-within:shadow pl-2">
             <input type="text" placeholder="Search products" className=" w-full outline-none "/>
             <div>
               <GrSearch  className="bg-red-700 h-full text-lg min-w-[50px] p-2 rounded-r-full text-white flex justify-center items-center"/>
             </div>
           </div>
 
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-7  ">
             <div className=" cursor-pointer text-3xl">
             <PiUser />
             </div>
             <div className="text-3xl relative">
-              <span>
+              <span className="flex">
                 <BsCart4 />
               </span>
              <div className="bg-red-700 text-white w-5 h-5 p-1 flex items-center justify-center rounded-full absolute -top-1 -right-2 ">
                <p className="text-sm">0</p>
              </div>
             </div>
+            <div className="">
+              <button className="px-3 bg-red-700 hover:bg-red-800  py-1 text-white cursor-pointer ">
+                Login
+              </button>
+            </div>
           </div>
+
        </div>
+
     </header>
 
     </>
