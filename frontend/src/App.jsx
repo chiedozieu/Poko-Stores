76af66a851082f9b48d8a9c428fcdaa1d318c256
chiedozieu <chiedozieugwor@gmail.com>
2024-06-20 08:07:52 +0100

    complete login ui, created signup, forgot password pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Login from './pages/Login'
+import ForgotPassword from './pages/ForgotPassword'
+import SignUp from './pages/SignUp'
 
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/login' element={<Login />}/>
+      <Route path='/forgot-password' element={<ForgotPassword />}/>
+      <Route path='/signup' element={<SignUp />}/>
     </Routes>
      <Footer />
    </BrowserRouter>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { LiaUserCircleSolid } from "react-icons/lia";
+import { PiUserCircleThin } from "react-icons/pi";
 import { TbEyeCheck, TbEyeClosed,  } from "react-icons/tb"; 
+import { Link } from "react-router-dom";
  
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -9,10 +10,11 @@ const Login = () => {
         <div className="mx-auto container p-4">
                 <div className="bg-white p-2 w-full max-w-md mx-auto py-5">
                     <div className="w-20 h-20 mx-auto">
-                        <LiaUserCircleSolid size={100}/>
+                        <PiUserCircleThin size={100} />
+
                     </div>
 
-                    <form action="">
+                    <form className="flex flex-col gap-10 p-4">
                         <div className="grid">
                             <label htmlFor="email">Email:</label>
                             <div className="bg-slate-100 p-2"><input type="email" placeholder="Enter email" className="w-fll h-full outline-none bg-transparent"/></div>
@@ -28,11 +30,16 @@ const Login = () => {
                                  }
                                  </span>
                                </div>
-                            </div>  
+                            </div> 
+                            <Link to='/forgot-password' className="text-sm cursor-pointer text-slate-500 block w-fit ml-auto mt-1 hover:underline hover:text- red-800">
+                            
+                                <p>Forgot password</p>
+                            </Link> 
                         </div>
 
-                        <button>Login</button>
+                        <button type="submit" className="p-2 w-full bg-red-700 hover:bg-red-800 cursor-pointer text-white text-md rounded-lg ">Login</button>
                     </form>
+                    <p className="p-4">Don't have account? <Link to='/signup' className="cursor-pointer text-red-700 hover:text-red-800 hover:underline">Sign Up</Link></p>
                 </div>
         </div>
 
